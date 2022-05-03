e07c342a959389b0cb2693bc1f8235080c314bc2
Levon <klevon1998@gmail.com>
2022-05-03 18:58:16 +0400

    Added skeleton of entity

---
+++ Engine/Engine.java
@@ -0,0 +1,5 @@
+package Engine;
+
+public class Engine {
+    // main engine of the game. will be responsible for the correct gameplay
+}
---
+++ Entity/Action.java
@@ -0,0 +1,9 @@
+package Entity;
+
+public class Action {
+    private String name;
+
+    public Action() {
+        name = null;
+    }
+}
