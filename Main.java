551efd86e54e26cc2f9d918bdaf820d517cdc6fa
suren-manukyan-aua <suren_manukyan@edu.aua.am>
2022-05-11 17:37:07 +0400

    Icon to start button

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Entity> entities = new ArrayList<Entity>(EnemyParser.getSelectedEnemies("Goblin"));
-        Player player = new Player("sdf");
+        Player player = new Player("Valod");
         entities.add(player);
         new BattleWindow(entities);
     }
